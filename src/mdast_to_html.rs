6a6fb082626a518213f665001fced430909b940d
CircuitSacul <me@circuitsacul.dev>
2025-05-24 01:55:25 -0400

    more stuff

--- src/mdast_to_html.rs
+++ src/mdast_to_html.rs
@@ -1,7 +1,20 @@
 use markdown::mdast;
 
-fn wrap(val: &mut String, elm: &str, class: &str) {
-    *val = format!("<{elm} class=\"{class}\">{val}</{elm}>");
+fn wrap(val: &mut String, elm: &str, attrs: &[(&str, &str)]) {
+    let attrs = attrs
+        .iter()
+        .map(|(k, v)| format!("{k}={v}"))
+        .collect::<Vec<_>>()
+        .join(" ");
+    *val = format!("<{elm} {attrs}>{val}</{elm}>");
+}
+
+fn warn(node: &mdast::Node) {
+    eprintln!("Warning: Unhandled node: {node:?}");
+}
+
+fn escape(text: &str) -> String {
+    text.replace("<", "&lt;").replace(">", "&gt;")
 }
 
 pub fn render(raw: &str, markdown: &mdast::Node) -> String {
@@ -18,7 +31,7 @@ fn process(raw: &str, node: &mdast::Node) -> String {
         for child in children {
             let child_pos = child.position().unwrap();
             if child_pos.start.offset > cursor {
-                output.push_str(&raw[cursor..child_pos.start.offset]);
+                output.push_str(&escape(&raw[cursor..child_pos.start.offset]));
             }
             cursor = child_pos.end.offset;
             let out = process(raw, child);
@@ -26,22 +39,54 @@ fn process(raw: &str, node: &mdast::Node) -> String {
         }
     }
     if cursor < pos.end.offset {
-        output.push_str(&raw[cursor..pos.end.offset]);
+        output.push_str(&escape(&raw[cursor..pos.end.offset]));
     }
 
     match node {
-        mdast::Node::Blockquote(_) => wrap(&mut output, "blockquote", ""),
-        mdast::Node::Heading(data) => {
-            let depth = data.depth;
-            output = format!("<h{depth}>{output}</h{depth}>");
-        }
-        mdast::Node::Paragraph(_) => wrap(&mut output, "p", ""),
-        mdast::Node::Emphasis(_) => output = format!("<em>{output}</em>"),
-        mdast::Node::Strong(_) => output = format!("<strong>{output}</strong>"),
-        mdast::Node::Code(_) => output = format!("<code>{output}</code>"),
-        mdast::Node::InlineCode(_) => wrap(&mut output, "kbd", ""),
-        mdast::Node::Delete(_) => output = format!("<del>{output}</del>"),
-        _ => (),
+        mdast::Node::Root(_) => (),
+
+        mdast::Node::Blockquote(_) => wrap(&mut output, "blockquote", &[]),
+        mdast::Node::FootnoteDefinition(_) => warn(node),
+        mdast::Node::MdxJsxFlowElement(_) => warn(node),
+        mdast::Node::List(data) => match data.ordered {
+            true => wrap(&mut output, "ol", &[]),
+            false => wrap(&mut output, "ul", &[]),
+        },
+
+        mdast::Node::MdxjsEsm(_) => warn(node),
+        mdast::Node::Toml(_) => warn(node),
+        mdast::Node::Yaml(_) => warn(node),
+
+        mdast::Node::Break(_) => (),
+        mdast::Node::InlineCode(_) => wrap(&mut output, "kbd", &[]),
+        mdast::Node::InlineMath(_) => warn(node),
+        mdast::Node::Delete(_) => wrap(&mut output, "del", &[]),
+        mdast::Node::Emphasis(_) => wrap(&mut output, "em", &[]),
+        mdast::Node::MdxTextExpression(_) => warn(node),
+        mdast::Node::FootnoteReference(_) => warn(node),
+        mdast::Node::Html(_) => warn(node),
+        mdast::Node::Image(_) => warn(node),
+        mdast::Node::ImageReference(_) => warn(node),
+        mdast::Node::MdxJsxTextElement(_) => warn(node),
+        mdast::Node::Link(data) => wrap(&mut output, "a", &[("href", &data.url)]),
+        mdast::Node::LinkReference(_) => warn(node),
+        mdast::Node::Strong(_) => wrap(&mut output, "strong", &[]),
+        mdast::Node::Text(_) => (),
+
+        mdast::Node::Code(_) => wrap(&mut output, "code", &[]),
+        mdast::Node::Math(_) => warn(node),
+        mdast::Node::MdxFlowExpression(_) => warn(node),
+        mdast::Node::Heading(data) => wrap(&mut output, &format!("h{}", data.depth), &[]),
+        mdast::Node::Table(_) => warn(node),
+        mdast::Node::ThematicBreak(_) => warn(node),
+
+        mdast::Node::TableRow(_) => warn(node),
+        mdast::Node::TableCell(_) => warn(node),
+
+        mdast::Node::ListItem(_) => wrap(&mut output, "li", &[]),
+
+        mdast::Node::Definition(_) => warn(node),
+        mdast::Node::Paragraph(_) => wrap(&mut output, "p", &[]),
     }
 
     output
